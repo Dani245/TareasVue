7c368fd4da0cbfc0859f7ed38ccf5162d9f4249d
Dani <Danilodepaz245@gmail.com>
2020-04-21 11:49:59 -0600

    agregue panels a tarea.js

--- componentes/tarea.js
+++ componentes/tarea.js
@@ -4,26 +4,26 @@ Vue.component("tarea", {
 	`
 	<div>
     <div class="mt-3" v-for="(tarea, index) of tareas">
-			<div :class="['alert', tarea.estado ? 'alert-success' : 'alert-danger']" role="alert">
-				<div class="d-flex justify-content-between align-items-center">
-					<div>
-						<h3>{{index}}. {{tarea.nombre}}</h3>
-						<p>{{tarea.descripcion}}</p>
-
-					</div>
-					<div>
-						<button class="btn btn-success btn-sm" @click="$emit('editar', $event, index)" v-if="!tareas[index].estado"
-							v-on:click="tareas[index].estado  = true">
-							OK
-						</button>
-						<button class="btn btn-danger btn-sm" @click="$emit('eliminar', $event, index)">
-							X
-						</button>
-					</div>
-				</div>
+		<div class="card text-white mb-3" :class="['bg', tarea.estado ? 'bg-success' : 'bg-danger']">
+			<div class="card-header">
+			  <h3>{{index}}. {{tarea.nombre}}</h3>
 			</div>
+  			<div class="card-body d-flex justify-content-between align-items-center">    			
+				<p>{{tarea.descripcion}}</p>				
+				<div>
+					<button class="btn btn-primary btn-sm" @click="$emit('editar', $event, index)" v-if="!tareas[index].estado"
+						v-on:click="tareas[index].estado  = true">
+						OK
+					</button>
+					<button class="btn btn-warning btn-sm" @click="$emit('eliminar', $event, index)">
+							X
+					</button>
+				</div>
+  			</div>
 		</div>
-		</div>
+
+	</div>
+	</div>
 	`,
 	props: {
 		tareas: Array,
