e0547135ce397b3d84fd095aaeebf20791607160
Dani <Danilodepaz245@gmail.com>
2020-04-23 12:25:22 -0600

    Agregue la información de mi perfil y agregue secciones

--- componentes/informacion.js
+++ componentes/informacion.js
@@ -3,25 +3,50 @@ Vue.component("info", {
     template:
 	`
     <div>
-        <div class="card">
-            <div class="card-header">
-            Información
+
+        <!-- Masthead-->
+        <header class="masthead bg-primary text-white text-center">
+            <div class="container d-flex align-items-center flex-column">
+                <img class="masthead-avatar mb-5" v-bind:src="imagen" alt="" />
+                <h1 class="masthead-heading mb-0">{{nombre}}</h1>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- Masthead Subheading-->
+                <p class="masthead-subheading font-weight-light mb-0">{{ocupacion[0]}} - {{ocupacion[1]}}</p>
             </div>
-            <div class="card-body">
-                <h3 class="card-title">{{proyecto}}</h3>
-                <hr>
-                <h4>{{nombre}}</h4>
-                <hr>
-                <h4>{{dpi}}</h4>
+        </header>
+        
+        <!-- About Section-->
+        <section class="page-section bg-primary text-white mb-0" id="about">
+            <div class="container">
+                <!-- About Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-white">Acerca</h2>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- About Section Content-->
+                <div class="row">
+                    <div class="col-lg-4 ml-auto"><p class="lead">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p></div>
+                    <div class="col-lg-4 mr-auto"><p class="lead">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p></div>
+                </div>                
             </div>
-        </div>        
-	</div>
+        </section>
+        
+    <div>
+        
 	`,
     data(){
         return{
             nombre: "José Danilo De Paz Martínez",
-            dpi: "3082901350608",
-            proyecto: "Proyecto Vue.js"
+            imagen: "assets/img/img.jpg",
+            ocupacion: ["Estudiante de Ingeniería en Sistemas","Programador y Diseñador Web"],
         }
     }
 });
