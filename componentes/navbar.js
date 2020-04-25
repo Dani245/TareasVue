ca205798f319e4aff0b7ecaa82fd4d32e96722ad
Dani <Danilodepaz245@gmail.com>
2020-04-25 08:59:08 -0600

    Le agregue el props y el v-for al componente navbar.js

--- componentes/navbar.js
+++ componentes/navbar.js
@@ -7,13 +7,14 @@ Vue.component("navbar", {
                 <a class="navbar-brand js-scroll-trigger" href="#app">Proyecto Vue</a><button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu <i class="fas fa-bars"></i></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index.html">Home</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="tareascompletadas.html">Tareas Completadas</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="acerca.html">Acerca</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" v-for="menuC of menu" v-bind:href="menuC.href">{{menuC.nombre}}</a></li>                    
                     </ul>
                 </div>
             </div>
         </nav>        
     </div>
-	`
+    `,
+    props: {
+		menu: Array,
+	}
 });
